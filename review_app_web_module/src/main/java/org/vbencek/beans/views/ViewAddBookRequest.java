659daf9bfc6341d74c7ebc6f12fd96fbd0f76c6c
Valentino Bencek <valentino.bencek12@gmail.com>
2021-07-07 21:10:18 +0200

    stayled add book request

--- review_app_web_module/src/main/java/org/vbencek/beans/views/ViewAddBookRequest.java
+++ review_app_web_module/src/main/java/org/vbencek/beans/views/ViewAddBookRequest.java
@@ -8,11 +8,16 @@ package org.vbencek.beans.views;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.Setter;
 import org.vbencek.beans.ActiveUserSession;
+import org.vbencek.localization.Localization;
 
 /**
  *
@@ -24,7 +29,39 @@ public class ViewAddBookRequest implements Serializable {
 
     @Inject
     ActiveUserSession activeUserSession;
-    
+
+    @Inject
+    Localization localization;
+    ResourceBundle res;
+
+    @Getter
+    @Setter
+    String isbn;
+
+    @Getter
+    @Setter
+    String bookTitle;
+
+    @Getter
+    @Setter
+    String bookDescription;
+
+    @Getter
+    @Setter
+    boolean renderReplyIsbn = false;
+
+    @Getter
+    @Setter
+    boolean renderReplyInfo = false;
+
+    @Getter
+    @Setter
+    String messageIsbn;
+
+    @Getter
+    @Setter
+    String messageInfo;
+
     @Getter
     @Setter
     boolean renderRedirect = false;
@@ -32,13 +69,70 @@ public class ViewAddBookRequest implements Serializable {
     @Getter
     @Setter
     String redirectFunction = "";
-    
+
     @PostConstruct
-    void init(){
+    void init() {
         if (false) { //activeUserSession.getActiveUser() == null
             redirectFunction = "location.href = 'index.xhtml';";
             renderRedirect = true;
+        } else {
+            setSearchParams();
         }
     }
-    
+
+    private void setSearchParams() {
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        isbn = params.get("ISBN");
+        bookTitle = params.get("BookTitle");
+        bookDescription = params.get("BookDescription");
+        System.out.println("ViewAddBookCollection: Opening view with parametars: "
+                + "ISBN: " + isbn + " "
+                + "BookTitle: " + bookTitle + " "
+                + "BookDescription: " + bookDescription);
+        if (isbn != null) {
+            checkEmptyISBNandSend();
+
+        } else if (bookTitle != null) {
+            checkEmptyInfoandSend();
+        }
+    }
+
+    private void checkEmptyISBNandSend() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        if (!"".equals(isbn.trim())) {
+            sendISBNBookRequest();
+        } else {
+            renderReplyIsbn = true;
+            messageIsbn =res.getString("viewAddBookRequest.messageRequiredISBN");
+        }
+    }
+
+    private void checkEmptyInfoandSend() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        if (!"".equals(bookTitle.trim())) {
+            sendInfoBookRequest();
+        } else {
+            renderReplyInfo = true;
+            messageInfo = res.getString("viewAddBookRequest.messageRequiredInfo");
+        }
+    }
+
+    private void sendISBNBookRequest() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        renderReplyIsbn = true;
+        if (true) {
+            messageIsbn = res.getString("viewAddBookRequest.messageISBN.ok");
+        } else {
+            messageIsbn = res.getString("viewAddBookRequest.messageISBN.notOk");
+        }
+        System.out.println("Sending: " + isbn);
+    }
+
+    private void sendInfoBookRequest() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        renderReplyInfo = true;
+        messageInfo = res.getString("viewAddBookRequest.messageInfo.ok");
+        System.out.println("Sending: " + bookTitle + " " + bookDescription);
+    }
+
 }

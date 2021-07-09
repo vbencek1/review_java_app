c1124dd9ef3ddff1d656ddee681db834783ced72
Valentino Bencek <48286606+vbencek1@users.noreply.github.com>
2021-07-09 19:32:10 +0200

    few changes

--- Database/sql_scripts/create_user_table.sql
+++ Database/sql_scripts/create_user_table.sql
@@ -9,3 +9,6 @@ CREATE TABLE USER_T (
     lastName varchar(255),
     email varchar(255) NOT NULL
 );
+
+--password is user
+INSERT INTO USER_T VALUES(default, 'user','214eb9f0829447645138d4daa96671099e55ab75180542dd792ef21761bac564af246707665cfef8','user','user','test.test@test.com');

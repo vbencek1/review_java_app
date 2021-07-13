9fe213ba61101186a7ce62bae7ca66dea66d1022
Valentino Bencek <48286606+vbencek1@users.noreply.github.com>
2021-07-13 18:23:54 +0200

    Add files via upload

--- Database/sql_scripts/create_user_table.sql
+++ Database/sql_scripts/create_user_table.sql
@@ -7,8 +7,10 @@ CREATE TABLE USER_T (
     password varchar(255) NOT NULL,
     firstName varchar(255),
     lastName varchar(255),
-    email varchar(255) NOT NULL
+    email varchar(255) NOT NULL,
+    isblocked boolean NOT NULL DEFAULT FALSE
 );
 
 --password is user
-INSERT INTO USER_T VALUES(default, 'user','214eb9f0829447645138d4daa96671099e55ab75180542dd792ef21761bac564af246707665cfef8','user','user','test.test@test.com');
+INSERT INTO USER_T VALUES(default, 'user','214eb9f0829447645138d4daa96671099e55ab75180542dd792ef21761bac564af246707665cfef8','user','user','test.test@test.com',default);
+INSERT INTO USER_T VALUES(default, 'user_blocked','214eb9f0829447645138d4daa96671099e55ab75180542dd792ef21761bac564af246707665cfef8','userBLKD','userBLKD','test.test1@test.com',true);

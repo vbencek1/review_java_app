7e49c6146a0a9cfc2418f2400f2279ff643ce580
Valentino Bencek <valentino.bencek12@gmail.com>
2021-07-15 22:47:40 +0200

    started working on backend for index

--- review_app_web_module/src/main/java/org/vbencek/beans/views/ViewHome.java
+++ review_app_web_module/src/main/java/org/vbencek/beans/views/ViewHome.java
@@ -8,17 +8,24 @@ package org.vbencek.beans.views;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.vbencek.beans.ActiveUserSession;
 import org.vbencek.beans.TestnaKlasaObrisati;
+import org.vbencek.facade.BookFacadeLocal;
+import org.vbencek.model.Book;
 import org.vbencek.properties.ParamsCaching;
 
 /**
@@ -28,16 +35,22 @@ import org.vbencek.properties.ParamsCaching;
 @Named(value = "viewHome")
 @ViewScoped
 public class ViewHome implements Serializable {
-    
+
+    @EJB(beanName = "BookFacade")
+    BookFacadeLocal bookFacade;
+
+    @Inject
+    ActiveUserSession activeUserSession;
+
     @Inject
     ParamsCaching paramsCaching;
-    
+
     @Getter
     @Setter
     List<TestnaKlasaObrisati> testList = new ArrayList<>();
 
     @PostConstruct
-    public void test() {
+    public void init() {
         testList.add(new TestnaKlasaObrisati(1, "Garry Lotter: part one", 2002, "Tino Rinek", "Izdavac _45", "Knjiga koja je dobra", 4.56, 20, new Date()));
         testList.add(new TestnaKlasaObrisati(2, "Larry Lotter: part two three", 2302, "Tino Vinek", "Izdavac _45", "Knjiga govori o covjeku", 3.56, 21, new Date()));
         testList.add(new TestnaKlasaObrisati(3, "Osman Lotter", 2012, "Tino Vinek", "Izdavac _45", "Neki opis knjige", 2.56, 200, new Date(1234124)));
@@ -46,29 +59,39 @@ public class ViewHome implements Serializable {
         testList.add(new TestnaKlasaObrisati(6, "Neka tamo knjiga test", 1002, "Tino Test", "Nekaj trece", "Svetlo na kraju tunela za mnoge ljudei", 5.00, 11, new Date(1234576)));
     }
 
-    public List<TestnaKlasaObrisati> getSixRecomendedbooks(int startingFrom, int totalNumber) {
-        List<TestnaKlasaObrisati> temp = new ArrayList<>();
-        int listEnd = startingFrom + totalNumber;
-        Collections.sort(testList, Comparator.comparingDouble(TestnaKlasaObrisati::getOcjena).reversed());
-        
-        if (testList.size() >= listEnd - 1) {
-            for (int i = startingFrom; i < listEnd; i++) {
-                temp.add(testList.get(i));
-            }
-        }
-        return temp;
+    public String convertDateToYear(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
+        simpleDateFormat = new SimpleDateFormat("YYYY");
+        return simpleDateFormat.format(date).toUpperCase();
     }
 
-    public List<TestnaKlasaObrisati> getSixPopularbooks(int startingFrom, int totalNumber) {
-        List<TestnaKlasaObrisati> temp = new ArrayList<>();
-        int listEnd = startingFrom + totalNumber;
-        Collections.sort(testList, Comparator.comparingInt(TestnaKlasaObrisati::getBrojOcjena).reversed());
-        if (testList.size() >= listEnd - 1) {
-            for (int i = startingFrom; i < listEnd; i++) {
-                temp.add(testList.get(i));
-            }
+    public String setIMG(Book book) {
+        if (book.getImgPath() != null) {
+            return book.getImgPath();
+        } else {
+            return "http://covers.openlibrary.org/b/isbn/" + book.getIsbn() + "-M.jpg";
         }
-        return temp;
+    }
+
+    public String makeTextShorter(String text, int maxChars) {
+        return StringUtils.abbreviate(text, maxChars);
+    }
+
+    public List<Book> getRecomendedbooks() {
+        if (activeUserSession.getActiveUser() == null) {
+            Random rand = new Random();
+            int rndOffset = rand.nextInt(100);
+            return  bookFacade.findBooksByCriteria("", "", 0, "", 0, "rating", rndOffset, 2);
+        } else {
+            //TO DO za sad nek bu defaultno
+            Random rand = new Random();
+            int rndOffset = rand.nextInt(100);
+            return  bookFacade.findBooksByCriteria("", "", 0, "", 0, "rating", rndOffset, 2);
+        }
+    }
+
+    public List<Book> getPopularbooks(int startingFrom, int totalNumber) {
+        return bookFacade.findBooksByCriteria("", "", 0, "", 0, "reviews", startingFrom, totalNumber);
     }
 
     public List<TestnaKlasaObrisati> getSixLatestbooks(int startingFrom, int totalNumber) {
@@ -81,10 +104,10 @@ public class ViewHome implements Serializable {
         }
         return temp;
     }
-    
-    public String redirectToBookDetails(int bookId){
+
+    public String redirectToBookDetails(int bookId) {
         paramsCaching.setBookIdCache(bookId);
-        return "bookDetails.xhtml?id="+bookId+"&faces-redirect=true";
+        return "bookDetails.xhtml?id=" + bookId + "&faces-redirect=true";
     }
 
 }

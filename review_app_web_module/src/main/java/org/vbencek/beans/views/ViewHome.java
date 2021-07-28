b26f3cc476516eec1635f2b4cde53eec0dbfe37f
Valentino Bencek <valentino.bencek12@gmail.com>
2021-07-28 18:56:28 +0200

    added logo, date sort on book (index), fixed few styling issues

--- review_app_web_module/src/main/java/org/vbencek/beans/views/ViewHome.java
+++ review_app_web_module/src/main/java/org/vbencek/beans/views/ViewHome.java
@@ -45,18 +45,9 @@ public class ViewHome implements Serializable {
     @Inject
     ParamsCaching paramsCaching;
 
-    @Getter
-    @Setter
-    List<TestnaKlasaObrisati> testList = new ArrayList<>();
-
     @PostConstruct
     public void init() {
-        testList.add(new TestnaKlasaObrisati(1, "Garry Lotter: part one", 2002, "Tino Rinek", "Izdavac _45", "Knjiga koja je dobra", 4.56, 20, new Date()));
-        testList.add(new TestnaKlasaObrisati(2, "Larry Lotter: part two three", 2302, "Tino Vinek", "Izdavac _45", "Knjiga govori o covjeku", 3.56, 21, new Date()));
-        testList.add(new TestnaKlasaObrisati(3, "Osman Lotter", 2012, "Tino Vinek", "Izdavac _45", "Neki opis knjige", 2.56, 200, new Date(1234124)));
-        testList.add(new TestnaKlasaObrisati(4, "Lome tro kl", 1111, "Maki Daki", "Neki tam izdavac", "Knjiga o necemu i svemu", 1.56, 24, new Date(123522344)));
-        testList.add(new TestnaKlasaObrisati(5, "Garry Lotter: part two", 2002, "Tino Tinek", "Izdavac _45", "Knjiga o svemu i govori sve", 1.52, 14, new Date(3456576)));
-        testList.add(new TestnaKlasaObrisati(6, "Neka tamo knjiga test", 1002, "Tino Test", "Nekaj trece", "Svetlo na kraju tunela za mnoge ljudei", 5.00, 11, new Date(1234576)));
+        //DO NOTHING FOR NOW
     }
 
     public String convertDateToYear(Date date) {
@@ -93,16 +84,9 @@ public class ViewHome implements Serializable {
     public List<Book> getPopularbooks(int startingFrom, int totalNumber) {
         return bookFacade.findBooksByCriteria("", "", 0, "", 0, "reviews", startingFrom, totalNumber);
     }
-
-    public List<TestnaKlasaObrisati> getSixLatestbooks(int startingFrom, int totalNumber) {
-        List<TestnaKlasaObrisati> temp = new ArrayList<>();
-        int listEnd = startingFrom + totalNumber;
-        if (testList.size() >= listEnd - 1) {
-            for (int i = startingFrom; i < listEnd; i++) {
-                temp.add(testList.get(i));
-            }
-        }
-        return temp;
+    
+    public List<Book> getLatestbooks(int startingFrom, int totalNumber) {
+        return bookFacade.findBooksByCriteria("", "", 0, "", 0, "date", startingFrom, totalNumber);
     }
 
     public String redirectToBookDetails(int bookId) {

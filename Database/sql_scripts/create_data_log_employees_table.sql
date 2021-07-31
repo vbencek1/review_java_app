86348f10f01ee7e0006d2549fa25c5d6f0d4e7ec
Valentino Bencek <48286606+vbencek1@users.noreply.github.com>
2021-07-31 13:47:17 +0200

    added new script, changed typo in data_log script

--- Database/sql_scripts/create_data_log_employees_table.sql
+++ Database/sql_scripts/create_data_log_employees_table.sql
@@ -1,5 +1,5 @@
-DROP TABLE DATA_LOG; 
-CREATE TABLE DATA_LOG (
+DROP TABLE DATA_LOG_EMPLOYEES; 
+CREATE TABLE DATA_LOG_EMPLOYEES (
     data_log_id int NOT NULL  primary key
         GENERATED ALWAYS AS IDENTITY
         (START WITH 1, INCREMENT BY 1),
@@ -7,8 +7,8 @@ CREATE TABLE DATA_LOG (
     method_name varchar(255),
     parametars varchar(1000),
     action_date timestamp NOT NULL,
-	user_id int NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES USER_T(user_id)
+	employee_id int NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
 );
 
-INSERT INTO DATA_LOG VALUES(default, 'test','test','test','2021-07-07 10:10:10',1);
+INSERT INTO DATA_LOG_EMPLOYEES VALUES(default, 'test','test','test','2021-07-07 10:10:10',1);

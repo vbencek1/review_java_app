a0a5ac7b7f078f10371d392ea2361296ffa39346
Valentino Bencek <valentino.bencek12@gmail.com>
2021-08-05 17:06:12 +0200

    finished all moderator actions

--- review_admin_app_web_module/src/main/java/org/vbencek/beans/views/ViewAdminModeratorDetails.java
+++ review_admin_app_web_module/src/main/java/org/vbencek/beans/views/ViewAdminModeratorDetails.java
@@ -5,9 +5,26 @@
  */
 package org.vbencek.beans.views;
 
+import java.io.IOException;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import lombok.Getter;
+import lombok.Setter;
+import org.vbencek.beans.ActiveUserSession;
+import org.vbencek.facade.EmployeeFacadeLocal;
+import org.vbencek.facade.EmployeeRoleFacadeLocal;
+import org.vbencek.localization.Localization;
+import org.vbencek.model.Employee;
+import org.vbencek.model.EmployeeRole;
 
 /**
  *
@@ -17,10 +34,143 @@ import java.io.Serializable;
 @ViewScoped
 public class ViewAdminModeratorDetails implements Serializable {
 
-    /**
-     * Creates a new instance of ViewAdminModeratorDetails
-     */
-    public ViewAdminModeratorDetails() {
+    @EJB(beanName = "EmployeeFacade")
+    EmployeeFacadeLocal employeeFacade;
+
+    @EJB(beanName = "EmployeeRoleFacade")
+    EmployeeRoleFacadeLocal employeeRoleFacade;
+
+    @Inject
+    ActiveUserSession activeUserSession;
+
+    @Inject
+    Localization localization;
+    ResourceBundle res;
+
+    //Atributes
+    @Getter
+    @Setter
+    String employeeUsername = "";
+    @Getter
+    @Setter
+    String employeeFirstname = "";
+    @Getter
+    @Setter
+    String employeeLastName = "";
+    @Getter
+    @Setter
+    String employeeEmail = "";
+    @Getter
+    @Setter
+    String employeePassword = "";
+    @Getter
+    @Setter
+    int employeeRoleId=2;
+
+    @Getter
+    @Setter
+    Employee thisEmployee;
+    
+    @Getter
+    @Setter
+    List<EmployeeRole> listEmployeeRole;
+
+    @PostConstruct
+    void init() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String employeeId = params.get("id");
+        int intEmployeeId = 0;
+        try {
+            intEmployeeId = Integer.parseInt(employeeId);
+        } catch (Exception e) {
+            System.out.println("ViewEmployeeDetails: Opening view to insert new employee");
+        }
+        if (intEmployeeId != 0) {
+            thisEmployee = employeeFacade.find(intEmployeeId);
+            if (thisEmployee != null) {
+                System.out.println("ViewEmployeeDetails: Opening view for employeeID: " + intEmployeeId);   
+            } else {
+                System.out.println("ViewEmployeeDetails: employeeID: " + intEmployeeId + " doesn't exist. Opening view to insert new employee");
+            }
+        }
+        setData();
+    }
+
+    public String setTitle() {
+        if (thisEmployee != null) {
+            return thisEmployee.getUsername();
+        } else {
+            return res.getString("admin.viewAdminModeratorDetails.title");
+        }
+    }
+
+    private void setData() {
+        listEmployeeRole=employeeRoleFacade.findAll();
+        if (thisEmployee != null) {
+            employeeUsername = thisEmployee.getUsername();
+            employeeFirstname = thisEmployee.getFirstname();
+            employeeLastName = thisEmployee.getLastname();
+            employeeEmail = thisEmployee.getEmail();
+            employeePassword = "";
+            employeeRoleId = thisEmployee.getEmployeeRoleId().getEmployeeRoleId();
+        }
     }
     
+    private void createEmployee() {
+        Employee employee = new Employee();
+        employee.setUsername(employeeUsername);
+        employee.setFirstname(employeeFirstname);
+        employee.setLastname(employeeLastName);
+        employee.setEmail(employeeEmail);
+        employee.setPassword(employeePassword);
+        employee.setEmployeeRoleId(employeeRoleFacade.find(employeeRoleId));
+        employee.setIsblocked(Boolean.FALSE);
+        employeeFacade.create(employee);
+        thisEmployee=employee;
+        activeUserSession.addDataLog(this.getClass().getSimpleName(), new Object() {
+        }.getClass().getEnclosingMethod().getName(), "EMPLOYEE ID: "+thisEmployee.getEmployeeId());
+    }
+
+    private void editEmployee() {
+        thisEmployee.setUsername(employeeUsername);
+        thisEmployee.setFirstname(employeeFirstname);
+        thisEmployee.setLastname(employeeLastName);
+        thisEmployee.setEmail(employeeEmail);
+        thisEmployee.setPassword(employeePassword);
+        thisEmployee.setEmployeeRoleId(employeeRoleFacade.find(employeeRoleId));
+        employeeFacade.edit(thisEmployee);
+        activeUserSession.addDataLog(this.getClass().getSimpleName(), new Object() {
+        }.getClass().getEnclosingMethod().getName(), "EMPLOYEE ID: "+thisEmployee.getEmployeeId());
+    }
+
+    private void redirect(String url) {
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void saveData() {
+        if (thisEmployee == null) {
+            createEmployee();
+        } else {
+            editEmployee();
+        }
+        String url = "adminModeratorDetails.xhtml?id=" + thisEmployee.getEmployeeId();
+        redirect(url);
+
+    }
+
+    public void refresh() {
+        String url;
+        if (thisEmployee != null) {
+             url = "adminModeratorDetails.xhtml?id=" + thisEmployee.getEmployeeId(); 
+        }else{
+             url = "adminModeratorDetails.xhtml?";
+        }
+        redirect(url);
+    }
+
 }
--- review_admin_app_web_module/src/main/java/org/vbencek/beans/views/ViewAdminModerators.java
+++ review_admin_app_web_module/src/main/java/org/vbencek/beans/views/ViewAdminModerators.java
@@ -5,9 +5,25 @@
  */
 package org.vbencek.beans.views;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.component.datatable.DataTable;
+import org.vbencek.beans.ActiveUserSession;
+import org.vbencek.facade.EmployeeFacadeLocal;
+import org.vbencek.localization.Localization;
+import org.vbencek.model.Employee;
 
 /**
  *
@@ -17,10 +33,60 @@ import javax.faces.view.ViewScoped;
 @ViewScoped
 public class ViewAdminModerators implements Serializable{
 
-    /**
-     * Creates a new instance of ViewAdminModerators
-     */
-    public ViewAdminModerators() {
+    @EJB(beanName = "EmployeeFacade")
+    EmployeeFacadeLocal employeeFacade;
+    
+    @Inject
+    ActiveUserSession activeUserSession;
+    
+    @Inject
+    Localization localization;
+    ResourceBundle res;
+    
+    @Getter
+    @Setter
+    List<Employee> listEmployee;
+    
+    @Getter
+    @Setter
+    Employee selectedEmployee;
+    
+    @Getter
+    @Setter
+    private SelectItem[] blockedOptions = new SelectItem[3];
+
+    @PostConstruct
+    public void init() {
+        res = ResourceBundle.getBundle("org.vbencek.localization.Translations", new Locale(localization.getLanguage()));
+        listEmployee = employeeFacade.findAll();
+        blockedOptions[0] = new SelectItem("", res.getString("admin.viewAdminModerators.combo.all"));
+        blockedOptions[1] = new SelectItem(Boolean.TRUE.toString(), res.getString("admin.viewAdminModerators.combo.blocked"));
+        blockedOptions[2] = new SelectItem(Boolean.FALSE.toString(), res.getString("admin.viewAdminModerators.combo.notBlocked"));
+    }
+    
+    public void unblockUser(Employee employee){
+        employee.setIsblocked(Boolean.FALSE);
+        employeeFacade.edit(employee);
+        activeUserSession.addDataLog(this.getClass().getSimpleName(), new Object(){}.getClass().getEnclosingMethod().getName(), "EMPLOYEE ID: "+employee.getEmployeeId());
+    }
+    
+    public void blockUser(Employee employee){
+        employee.setIsblocked(Boolean.TRUE);
+        employeeFacade.edit(employee);
+        activeUserSession.addDataLog(this.getClass().getSimpleName(), new Object(){}.getClass().getEnclosingMethod().getName(), "EMPLOYEE ID: "+employee.getEmployeeId());
+    }
+    
+    public void redirectToModDetails(){
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("adminModeratorDetails.xhtml?id=" + selectedEmployee.getEmployeeId());
+        } catch (IOException ex) {
+            System.out.println("ViewAdminModerator: can't redirect");
+        }
+    }
+    
+    public void resetvalues() {
+        DataTable datatable = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent(":formEmployees:employees");
+        datatable.reset();
     }
     
 }
